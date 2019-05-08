8b18fbb85dd4b585559d00a68d604eb0b27bd1c2
Cleo "Lo" Howard <lolearningcode@gmail.com>
2019-05-08 07:31:35 -0600

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Lo Howard on 5/8/19.
+//  Copyright © 2019 Lo Howard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
